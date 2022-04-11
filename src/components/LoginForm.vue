8347e243c507ed8068e833d36463d6b508616fe6
unknown <jordan.barrett.jm@gmail.com>
2022-04-10 21:13:08 -0500

    fixed csrf token not saving

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,7 +4,7 @@
         <div  v-if="isSuccess">
              </div>
 
-        <form @submit.prevent="loginUser" id="LoginForm" method="POST" enctype="multipart/form-data">
+        <form @submit.prevent="loginUser" id="loginForm">
         <div class="form-group">
         
             <label> Username </label><br>
@@ -14,7 +14,7 @@
             <input type="password" name="password"><br>
 
         </div>
-        <button class="btn btn-primary mb-2" > Login </button>
+        <button type="submit" class="btn btn-primary mb-2" > Login </button>
         </form>
     </div>
 </template>
@@ -23,54 +23,53 @@
 export default {
     name: 'LoginForm',
     data() {
-        return{
-            errors: [],
-            success: false,
-            token: ''
-        };
-    },
-     created() {
-            this.getCsrfToken();
-     },
-    methods:{
+            return {
+                csrf_token: ''
+            };
+    },methods:{
         loginUser(){
             let loginForm = document.getElementById('loginForm');
             let form_data = new FormData(loginForm);
-            fetch("/api/auth/login", {
+            console.log(this.errors)
+            fetch("/api/login", {
                 method: 'POST',
                 body: form_data,
                 headers: {
-                    'X-CSRFToken': this.token
-                     },
-                     credentials: 'same-origin'
-            })
-               .then(function (response) {
-                return response.json();
+                    'X-CSRFToken': this.csrf_token
+                }
                 })
-                .then(function (data) {
-                    if ('errors' in data){
-                        this.isSuccess = false
-                    }
-                    
-                //this.successmessage = "File Uploaded Successfully"
-                // display a success message
-                console.log(jsonResponse);
+               .then(function (response) {
+                    return response.json();
+                    })
+                    .then(function (data) {
+                        if ('errors' in data){
+                            this.isSuccess = false
+                        }
+                        
+                    //this.successmessage = "File Uploaded Successfully"
+                    // display a success message
+                    console.log(jsonResponse);
                 })
                 .catch(function (error) {
                 //this.errormessage = "Something went wrong"
                 console.log(error);
                 });
             },
-             getCsrfToken() {
+            getCsrfToken() {
                 let self = this;
                 fetch('/api/csrf-token')
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data);
-                        self.csrf_token = data.csrf_token;
-                    })
-             },
-    }
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    self.csrf_token = data.token;
+                    console.log(self.csrf_token);
+                    console.log(self.other_data)
+                })
+            }
+    },
+     created() {
+            this.getCsrfToken();
+     }
 }
 </script>
 
