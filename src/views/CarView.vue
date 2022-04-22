5208828fde0602a58a4f04a26ff4a8173ac7fd54
Itawnya Walker <itawnya.walker@gmail.com>
2022-04-22 14:27:41 -0500

    Update- Single Car View

---
+++ src/views/CarView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <CarInfo />
+    </div>
+</template>
+
+<script>
+    import CarInfo from '@/components/CarInfo.vue'
+
+    export default{
+        data() {
+            return{};
+        },
+        components: { CarInfo }
+    };
+</script>
